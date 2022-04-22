abfdf5e1de68aeca2bc29d49e5031ad138479e8a
hisho <hisho.web@gmail.com>
2022-04-23 04:10:38 +0900

    ✨ create PokemonCard

---
+++ src/component/feature/Pokemon/PokemonCard/PokemonCard.generate.graphql.ts
@@ -0,0 +1,6 @@
+import * as Types from '../../../../lib/graphql/$types';
+
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type PokemonCard_PokemonFragment = { __typename?: 'Pokemon', name?: string | null, image?: string | null, types?: Array<string | null> | null };
+
+export const PokemonCard_PokemonFragmentDoc = {"kind":"Document","definitions":[{"kind":"FragmentDefinition","name":{"kind":"Name","value":"PokemonCard_pokemon"},"typeCondition":{"kind":"NamedType","name":{"kind":"Name","value":"Pokemon"}},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"image"}},{"kind":"Field","name":{"kind":"Name","value":"types"}}]}}]} as unknown as DocumentNode<PokemonCard_PokemonFragment, unknown>;
