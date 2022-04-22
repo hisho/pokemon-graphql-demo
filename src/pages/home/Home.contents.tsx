1519bc314a80b60fc8fc7853d36ae7ae9e3e8121
hisho <hisho.web@gmail.com>
2022-04-23 04:10:47 +0900

    ✨ create PokemonCard

--- src/pages/home/Home.contents.tsx
+++ src/pages/home/Home.contents.tsx
@@ -1,8 +1,7 @@
 import { useQuery } from '@apollo/client'
 import { PokemonsDocument } from '@src/component/feature/Pokemon/PokemonsList/PokemonsList.generate.graphql'
-import { Box, Text, Title } from '@mantine/core'
-import { Navigate } from '@src/component/functional/Navigate/Navigate'
 import { PokemonsList } from '@src/component/feature/Pokemon/PokemonsList/PokemonsList'
+import { PokemonCard } from '@src/component/feature/Pokemon/PokemonCard/PokemonCard'
 
 /**
  * contents
@@ -13,31 +12,13 @@ export const HomeContents = () => {
   return (
     <PokemonsList data={data} isLoading={isLoading} error={error}>
       {(pokemons) =>
-        pokemons.map((pokemon) => (
-          <Box key={`pokemons_${pokemon?.id}`}>
-            <Text>{pokemon?.id}</Text>
-            <Title>{pokemon?.name}</Title>
-            {pokemon?.image && <img src={pokemon?.image} alt="" />}
-            <Box
-              sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                gap: '8px',
-              }}
-            >
-              {pokemon?.types?.map((type) => (
-                <Text>{type}</Text>
-              ))}
-            </Box>
-            <Navigate
-              href={(path) =>
-                path.pokemon._pokemon_name(pokemon?.name ?? 'null').$url()
-              }
-            >
-              <a>詳細</a>
-            </Navigate>
-          </Box>
-        ))
+        pokemons.map((pokemon) =>
+          pokemon ? (
+            <PokemonCard key={`pokemonCard_${pokemon.id}`} {...pokemon} />
+          ) : (
+            <></>
+          )
+        )
       }
     </PokemonsList>
   )
