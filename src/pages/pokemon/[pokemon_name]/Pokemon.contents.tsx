f179398c8969f5028049c91f894cf145d7598200
hisho <hisho.web@gmail.com>
2022-04-23 06:03:23 +0900

    ✨ add PokemonCard & pokemon query to Pokemon.contents.tsx

--- src/pages/pokemon/[pokemon_name]/Pokemon.contents.tsx
+++ src/pages/pokemon/[pokemon_name]/Pokemon.contents.tsx
@@ -1,13 +1,31 @@
+import { Button } from '@mantine/core'
+import { useRouter } from 'next/router'
+import { Navigate } from '@src/component/functional/Navigate/Navigate'
+import { useQuery } from '@apollo/client'
+import { PokemonDocument } from '@src/component/feature/Pokemon/PokemonCard/pokemon.generate.graphql'
+import { PokemonCard } from '@src/component/feature/Pokemon/PokemonCard/PokemonCard'
+
 /**
  * contents
  */
-import { useRouter } from 'next/router'
-
 export const PokemonContents = () => {
   const { query } = useRouter()
   const pokemonName =
     typeof query['pokemon_name'] === 'string'
       ? query['pokemon_name']
       : undefined
-  return <>syousai</>
+
+  const { data } = useQuery(PokemonDocument, {
+    variables: {
+      name: pokemonName,
+    },
+  })
+  return (
+    <>
+      {data && <PokemonCard {...data.pokemon} />}
+      <Navigate href={(path) => path.home.$url()}>
+        <Button component={'a'}>一覧に戻る</Button>
+      </Navigate>
+    </>
+  )
 }
