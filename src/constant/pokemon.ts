a7f50fbac96df2e37d0915f33af383713ff79263
hisho <hisho.web@gmail.com>
2022-04-24 03:59:34 +0900

    ✨ create pokemon constant

---
+++ src/constant/pokemon.ts
@@ -0,0 +1,26 @@
+export const POKEMON_TYPE_COLOURS = {
+  bug: '#A6B91A',
+  dark: '#705746',
+  dragon: '#6F35FC',
+  electric: '#F7D02C',
+  fairy: '#D685AD',
+  fighting: '#C22E28',
+  fire: '#EE8130',
+  flying: '#A98FF3',
+  ghost: '#735797',
+  grass: '#7AC74C',
+  ground: '#E2BF65',
+  ice: '#96D9D6',
+  normal: '#A8A77A',
+  poison: '#A33EA1',
+  psychic: '#F95587',
+  rock: '#B6A136',
+  steel: '#B7B7CE',
+  water: '#6390F0',
+} as const
+
+export const getPokemonTypeColours = (type: string) => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return POKEMON_TYPE_COLOURS[type.toLocaleLowerCase()] ?? '#fff'
+}
