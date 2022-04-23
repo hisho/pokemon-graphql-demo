c1848093ff333bfc62fae4be047ac29ca2a994d5
hisho <hisho.web@gmail.com>
2022-04-23 23:37:47 +0900

    🎨 format

--- src/component/feature/Pokemon/PokemonCard/PokemonCard.tsx
+++ src/component/feature/Pokemon/PokemonCard/PokemonCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Image } from '@mantine/core'
+import { AspectRatio, Box, Image } from '@mantine/core'
 import { PokemonCard_PokemonFragment } from '@src/component/feature/Pokemon/PokemonCard/PokemonCard.generate.graphql'
 
 export const PokemonCard = ({
@@ -12,9 +12,9 @@ export const PokemonCard = ({
       <Box>{number}</Box>
       <Box>{name}</Box>
       {image && (
-        <Box>
-          <Image src={image} />
-        </Box>
+        <AspectRatio mx={'auto'} ratio={1} sx={{ maxWidth: '600px' }}>
+          <Image fit={'contain'} src={image} />
+        </AspectRatio>
       )}
       <Box>
         {types?.map((type) => (
